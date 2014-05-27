2b4adbd0e6324b25f7e087b7163e9c63496965ef
taikiken <denkosekka@gmail.com>
2014-05-27 18:21:39 +0900

    test code

--- src/Sagen.js
+++ src/Sagen.js
@@ -8,6 +8,9 @@
  * Distributed under the terms of the MIT license.
  * http://www.opensource.org/licenses/mit-license.html
  *
+ *
+ *
+ *
  * This notice shall be included in all copies or substantial portions of the Software.
  */
 
