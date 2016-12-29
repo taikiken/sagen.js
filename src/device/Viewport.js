5456ad91f668671d6d6928643d7207740b8129e4
taikiken <o@parachute-canopy.org>
2016-12-30 03:18:21 +0900

    Viewpoer update #3

--- src/device/Viewport.js
+++ src/device/Viewport.js
@@ -17,234 +17,180 @@
  * @module Sagen
  * @submodule Viewport
  * */
-( function ( window ){
+(function(window) {
   'use strict';
-
   var
     document = window.document,
-    Sagen = window.Sagen;
+    Sagen = window.Sagen,
 
-  Sagen.Viewport = ( function (){
-
-    var
-      Browser = Sagen.Browser,
-      iOS = Browser.iOS,
-      Element = Browser.Element,
-      /**
-       * @property _viewport
-       * @static
-       * @type {HTMLElement|String}
-       * @private
-       */
-      _viewport,
-      /**
-       * @property _content
-       * @static
-       * @type {string}
-       * @private
-       */
-      _content;
-
+    Browser = Sagen.Browser,
+    iOS = Browser.iOS,
+    Element = Browser.Element,
     /**
-     * @class Viewport
+     * @property viewport
      * @static
-     * @constructor
+     * @type {HTMLElement|String}
+     * @private
      */
-    function Viewport () {
-      throw new Error( 'Viewport can\'t create instance.' );
+    viewport,
+    /**
+     * @property content
+     * @static
+     * @type {string}
+     * @private
+     */
+    content;
+
+  /**
+   * viewport 関連処理を行います
+   * @class Viewport
+   * @static
+   * @constructor
+   */
+  function Viewport() {
+    throw new Error('Viewport can\'t create instance.');
+  }
+
+  var p = Viewport.prototype;
+  p.constructor = Viewport;
+
+  /**
+   * data-ios が true の時に minimalUi を実行します
+   * @method init
+   * @static
+   */
+  Viewport.init = function() {
+    if (Sagen.dataSet('ios')) {
+      Viewport.minimalUi();
+    }
+  };
+  /**
+   * viewport tag と viewport.content を取得します
+   * @method find
+   * @static
+   * @return {Viewport} method chain 可能にします
+   */
+  Viewport.find = function() {
+    if (typeof viewport === 'undefined') {
+      // viewport undefined
+      viewport = Element.find('meta[name=\'viewport\']');
     }
 
-    var p = Viewport.prototype;
-
-    p.constructor = Viewport;
-
-    /**
-     * @method init
-     * @static
-     */
-    Viewport.init = function () {
-
-      if ( Sagen.dataSet( 'ios' ) ) {
-
-        Viewport.minimalUi();
-
-      }
-
-    };
-    /**
-     * @method find
-     * @static
-     * @return {Viewport}
-     */
-    Viewport.find = function () {
-
-      if ( typeof _viewport === 'undefined' ) {
-
-        // _viewport undefined
-        _viewport = Element.find( 'meta[name=\'viewport\']' );
-
-      }
-
-      if ( !!_viewport ) {
-
-        _content = _viewport.content;
-
-      } else {
-
-        _viewport = '';
-        _content = '';
-
-      }
-
-      return Viewport;
-
-    };
-    /**
-     * @method Viewport
-     * @static
-     * @return {HTMLElement|*}
-     */
-    Viewport.meta = function () {
-
-      Viewport.find();
-      return _viewport;
-
-    };
-    /**
-     * @method content
-     * @static
-     * @return {string}
-     */
-    Viewport.content = function () {
-
-      Viewport.find();
-      return _content;
-
-    };
-    /**
-     * @method write
-     * @static
-     * @param {String} content
-     * @return {Viewport}
-     */
-    Viewport.write = function ( content ) {
-
-      var
-        meta;
-
-      Viewport.find();
-
-      if ( !_viewport ) {
-
-        meta = document.createElement( 'meta' );
-        meta.name = 'viewport';
-        meta.content = content;
-        _viewport = meta;
-        _content = content;
-        document.getElementsByTagName( 'head' )[ 0 ].appendChild( meta );
-
-      }
-
-      return Viewport;
-
-    };
-    /**
-     * @method add
-     * @static
-     * @param {string} option
-     * @return {Viewport}
-     */
-    Viewport.add = function ( option ) {
-
-      var
-        content;
-
-      Viewport.find();
-
-      if ( !!_viewport ) {
-
-        content = _viewport.content;
-
-        if ( content.indexOf( option ) === -1 ) {
-
-          _viewport.content = content + ', ' + option;
-
-        }
-
-      }
-
-      return Viewport;
-
-    };
-    /**
-     * @method replace
-     * @static
-     * @param {string} oldOption
-     * @param {string} newOption
-     * @return {Viewport}
-     */
-    Viewport.replace = function ( oldOption, newOption ) {
-
-      var
-        content;
-
-      Viewport.find();
-
-      if ( !!_viewport ) {
-
-        content = _viewport.content;
-
-        if ( content.indexOf( oldOption ) !== -1 ) {
-
-          content.split( oldOption ).join( newOption );
-          _viewport.content = content;
-
-        }
-
-      }
-
-      return Viewport;
-
-    };
-
-    /**
-     * viewport content 引数で書換
-     * @method rewrite
-     * @static
-     * @param {string} content
-     * @return {Viewport}
-     */
-    Viewport.rewrite = function ( content ) {
-
-      Viewport.find();
-
-      if ( !!_viewport ) {
-
-        _viewport.content = content;
-
-      }
-
-      return Viewport;
-
-    };
-    /**
-     * @method minimalUi
-     * @static
-     */
-    Viewport.minimalUi = function () {
-
-      var version = iOS.version();
-
-      if ( version >= 7.1 && version < 8.0 ) {
-
-        Viewport.add( 'minimal-ui' );
-
-      }
-
-    };
+    if (!!viewport) {
+      content = viewport.content;
+    } else {
+      viewport = '';
+      content = '';
+    }
 
     return Viewport;
+  };
+  /**
+   * viewport tag を取得します
+   * @method Viewport
+   * @static
+   * @return {HTMLElement|*} viewport tag
+   */
+  Viewport.meta = function() {
+    Viewport.find();
+    return viewport;
+  };
+  /**
+   * viewport.content を取得します
+   * @method content
+   * @static
+   * @return {string} viewport.content
+   */
+  Viewport.content = function() {
+    Viewport.find();
+    return content;
+  };
+  /**
+   * viewport tag を書き込みます
+   * @method write
+   * @static
+   * @param {String} contentOption viewport.content データ
+   * @return {Viewport} method chain 可能にします
+   */
+  Viewport.write = function(contentOption) {
+    var
+      meta;
+    Viewport.find();
+    if (!viewport) {
+      meta = document.createElement('meta');
+      meta.name = 'viewport';
+      meta.content = contentOption;
+      viewport = meta;
+      content = contentOption;
+      document.getElementsByTagName('head')[0].appendChild(meta);
+    }
+    return Viewport;
+  };
+  /**
+   * viewport に追加します
+   * @method add
+   * @static
+   * @param {string} option 追加オプション
+   * @return {Viewport} method chain 可能にします
+   */
+  Viewport.add = function(option) {
+    var
+      contentOption;
+    Viewport.find();
+    if (!!viewport) {
+      contentOption = viewport.content;
+      if (contentOption.indexOf(option) === -1) {
+        viewport.content = contentOption + ', ' + option;
+      }
+    }
+    return Viewport;
+  };
+  /**
+   * viewport を一部書き換えます
+   * @method replace
+   * @static
+   * @param {string} oldOption 置換え元
+   * @param {string} newOption 置換える viewport
+   * @return {Viewport} method chain 可能にします
+   */
+  Viewport.replace = function(oldOption, newOption) {
+    var
+      contentOption;
+    Viewport.find();
+    if (!!viewport) {
+      contentOption = viewport.content;
+      if (contentOption.indexOf(oldOption) !== -1) {
+        contentOption.split(oldOption).join(newOption);
+        viewport.content = contentOption;
+      }
+    }
+    return Viewport;
+  };
+  /**
+   * viewport content 引数で書換
+   * @method rewrite
+   * @static
+   * @param {string} contentOption 新規 content 文字列
+   * @return {Viewport} method chain 可能にします
+   */
+  Viewport.rewrite = function(contentOption) {
+    Viewport.find();
+    if (!!viewport) {
+      viewport.content = contentOption;
+    }
+    return Viewport;
+  };
+  /**
+   * `minimal-ui` を追加します, iOS 7.1 ~ 8.0未満の時に実行されます
+   * @method minimalUi
+   * @static
+   */
+  Viewport.minimalUi = function() {
+    var version = iOS.version();
+    if (version >= 7.1 && version < 8.0) {
+      Viewport.add('minimal-ui');
+    }
+  };
 
-  }() );
-
+  Sagen.Viewport = Viewport;
 }( window ) );
